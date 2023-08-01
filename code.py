2696478eba0a307d97b438bab13eb55f696a533b
panizo126214 <panizo.126214@e.unavarra.es>
2023-08-01 16:42:42 +0200

    Automatic access to terminal and ls

--- code.py
+++ code.py
@@ -5,23 +5,37 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-
+print("Device is working!")
 keyboard = Keyboard(usb_hid.devices)
 
-mute_pin = board.GP16
+terminal_pin = board.GP16
 
 # Initializing Button
-mute = digitalio.DigitalInOut(mute_pin)
-mute.direction = digitalio.Direction.INPUT
-mute.pull = digitalio.Pull.DOWN
-mute_bool = False
+terminal = digitalio.DigitalInOut(terminal_pin)
+terminal.direction = digitalio.Direction.INPUT
+terminal.pull = digitalio.Pull.DOWN
+terminal_bool = False
 
 
 while True:
     
-    if mute.value:  
+    if terminal.value:  
         print("Reloading")
-        keyboard.press(Keycode.F5)
+        keyboard.press(227) #Simulate pressing Windows
         time.sleep(0.15)
-        keyboard.release(Keycode.F5)
-        mute_bool = not mute_bool
+        keyboard.press(21) #Simulate pressing R
+        time.sleep(0.15)
+        keyboard.release(227)
+        time.sleep(0.15)
+        keyboard.release(21)
+        
+        #Writting wt.exe on panel to get terminal access
+        keyboard.send(26,23,55,8)
+        keyboard.send(27,8,40)
+        time.sleep(2) #Wait 2 secs in order to let terminal open
+        print("Granted terminal access!")
+        
+        #Now write ls command and execute it
+        keyboard.send(15,22,40)
+
+        terminal_bool = not terminal_bool
